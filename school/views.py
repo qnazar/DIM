7c6636e1ca13b084f6d2f54784eb896c3115a96e
Nazar Klypych <xxxnazarkoxxx@gmail.com>
2023-01-26 18:47:53 +0200

    Configured the static files

--- school/views.py
+++ school/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('DIM')
+    return render(request, 'school/home.html', context={'test': 'Static'})
